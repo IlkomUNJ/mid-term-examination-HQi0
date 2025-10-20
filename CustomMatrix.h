ad88a8d2be22ba267a5ec6352826181412183377
HQi0 <150333456+HQi0@users.noreply.github.com>
2025-10-20 15:21:41 +0700

    Update CustomMatrix.h

--- CustomMatrix.h
+++ CustomMatrix.h
@@ -55,6 +55,17 @@ public:
         cout << "" << endl;
     }
 
+    bool operator==(const CustomMatrix& other) const {
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < 3; ++j) {
+                if (this->mat[i][j] != other.mat[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
 };
 
 #endif // CUSTOMMATRIX_H
