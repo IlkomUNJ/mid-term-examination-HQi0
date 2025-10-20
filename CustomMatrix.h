cf7d991f5f4d9485a9791b2fb0d59840ff96109b
HQi0 <150333456+HQi0@users.noreply.github.com>
2025-10-20 15:06:47 +0700

    Update CustomMatrix.h

--- CustomMatrix.h
+++ CustomMatrix.h
@@ -14,9 +14,9 @@ public:
 
     //alternate way to instantiate the class
     CustomMatrix(bool m[3][3]){
-        const int m_size = sizeof(m) / sizeof(m[0]);
-        for (int i=0;i< m_size;i++) {
-            for(int j=0; j < sizeof(m[0]);j++){
+        // const int m_size = sizeof(m) / sizeof(m[0]);
+        for (int i=0;i< 3;i++) {
+            for(int j=0; j < 3;j++){
                 //cout << i << " " << j << endl;
                 mat[i][j] = m[i][j];
             }
@@ -24,14 +24,37 @@ public:
     }
 
     void fillMatrix(bool m[3][3]){
-        const int m_size = sizeof(m) / sizeof(m[0]);
-        for (int i=0;i< m_size;i++) {
-            for(int j=0; j < sizeof(m[0]);j++){
+        // const int m_size = sizeof(m) / sizeof(m[0]);
+        for (int i=0;i< 3;i++) {
+            for(int j=0; j < 3;j++){
                 //cout << i << " " << j << endl;
                 mat[i][j] = m[i][j];
             }
         }
     }
+
+    bool isEmpty(){
+        for (int i=0;i< 3;i++) {
+            for(int j=0; j < 3;j++){
+                if(mat[i][j] == true){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    void print(){
+        cout << "" << endl;
+        for (int i=0; i < 3; i++) {
+            for(int j=0; j < 3; j++){
+                cout << (mat[i][j] ? "1" : "0") << " ";
+            }
+            cout << endl;
+        }
+        cout << "" << endl;
+    }
+
 };
 
 #endif // CUSTOMMATRIX_H
