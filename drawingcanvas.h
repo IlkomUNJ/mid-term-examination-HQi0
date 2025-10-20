736c2d5265be69e8bc089a623c339f7a50aa3a39
HQi0 <150333456+HQi0@users.noreply.github.com>
2025-10-20 15:21:19 +0700

    Update drawingcanvas.h

--- drawingcanvas.h
+++ drawingcanvas.h
@@ -21,26 +21,28 @@ class DrawingCanvas : public QWidget
 private:
     const int WINDOW_WIDTH=600;
     const int WINDOW_HEIGHT=400;
-
+    
 public:
     explicit DrawingCanvas(QWidget *parent = nullptr);
-
+    
     // Slot to clear all points from the canvas
     void clearPoints();
     void paintLines();
     void segmentDetection();
-
+    
 protected:
     // Overridden method to handle painting on the widget
     void paintEvent(QPaintEvent *event) override;
-
+    
     // Overridden method to handle mouse clicks
     void mousePressEvent(QMouseEvent *event) override;
-
+    
 private:
     // A vector to store all the points drawn by the user
     QVector<QPoint> m_points;
-
+    
+    QVector<QPoint> m_candidates;
+    
     bool isPaintLinesClicked = false;
 };
 #endif // DRAWINGCANVAS_H
